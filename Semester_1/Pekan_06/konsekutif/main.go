1679dc12762bb4cbad0a52d7998af86cff80dd1c
siunlucky <faizelfahad2@gmail.com>
2023-11-03 09:19:04 +0700

    update konsekutif

--- Semester_1/Pekan_06/konsekutif/main.go
+++ Semester_1/Pekan_06/konsekutif/main.go
@@ -8,16 +8,16 @@ func main()  {
 	var x, lastDigit, digitNow int;
 	var isOne bool;
 
-	fmt.Scan(&x);
+	fmt.Scan(&x); // 12345
 
 	isOne = true;
 	
 	for x > 0 && isOne == true {
-		lastDigit = x % 10;
-		x /= 10;
-		digitNow = x % 10
-		isOne = (lastDigit - digitNow == 1) || (digitNow - lastDigit == 1);
-		x /= 10
+		lastDigit = x % 10; // 12345 % 10 = 5
+		x /= 10; // 12345 / 10 = 1234
+		digitNow = x % 10 // 1234 % 10 = 4
+		isOne = (lastDigit - digitNow == 1) || (digitNow - lastDigit == 1); // (5 - 4 == 1) || (4 - 5 == 1) = True
+		x /= 10 // 1234 / 10 = 123
 	}
 
 	fmt.Println(isOne);
